fe4e83f8270eb400e64403278dc9a3524a72d9f6
uomma <marcelloleone975@gmail.com>
2023-12-07 22:34:45 +0100

    v-for

--- js/main.js
+++ js/main.js
@@ -20,13 +20,28 @@ button.addEventListener('input', function(event){
 
 const { createApp } = Vue
 //option API
-createApp({
+const vm = createApp({
     data() {
         return {
+
+            nLoops: 23,
             message: 'Hello Vue!',
             subtitle: 'sotto titolo!',
             friends: ['carlo', 'marco ', 'luca'],
             awesome: true,
+            lista: [{
+                nome: 'patrick',
+                eta: 300,
+                faDomande: true
+            }, {
+                nome: 'mr crab',
+                eta: 90,
+                faDomande: false
+            },
+            ],
+            mode: 1,
+            img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d0/Apple_logo_Think_Different_vectorized.svg/1920px-Apple_logo_Think_Different_vectorized.svg.png',
+            showImg: true
         }
     },
     methods: {
@@ -34,14 +49,14 @@ createApp({
             return 'ciao'
         } */
 
-        generateRandom(){
-              return Math.floor(Math.random() * 10) + 1;
+        generateRandom() {
+            return Math.floor(Math.random() * 10) + 1;
         },
         greetings() {
             console.log('ho cliccato sul bottone')
             this.message = 'nuovo messaggio';
-            this.friends.push(this.generateRandom(1,100))
-            console.log(this. friends)
+            this.friends.push(this.generateRandom(1, 100))
+            console.log(this.friends)
         }
 
     },
