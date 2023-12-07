0608cfe15049701bfb606988a7bc1f69f0ac6765
uomma <marcelloleone975@gmail.com>
2023-12-08 00:35:03 +0100

    methods

--- js/main.js
+++ js/main.js
@@ -23,8 +23,17 @@ const { createApp } = Vue
 const vm = createApp({
     data() {
         return {
+            pageTitle: 'sono il titolo della pagina',
 
-            nLoops: 23,
+            movie: {
+                imgaes: [
+                    'https://leganerd.com/wp-content/uploads/2013/04/20130425-083038.jpg',
+                    'https://pad.mymovies.it/filmclub/2012/06/004/locandina.jpg',
+                    'https://www.filmtv.it/imgbank/GALLERYXL/red/iron_man_3_poster_ita_2.jpg'], title: 'Ironman 3',
+                category: 'action',
+                length: '150',
+            }
+            /* nLoops: 23,
             message: 'Hello Vue!',
             subtitle: 'sotto titolo!',
             friends: ['carlo', 'marco ', 'luca'],
@@ -41,23 +50,34 @@ const vm = createApp({
             ],
             mode: 1,
             img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d0/Apple_logo_Think_Different_vectorized.svg/1920px-Apple_logo_Think_Different_vectorized.svg.png',
-            showImg: true
+            showImg: true */
         }
     },
     methods: {
+        nextImg() {
+            
+        }, 
+
+        prevImg() {
+           
+        }
+
+
+
+
         /* greetings : function(){
             return 'ciao'
         } */
 
-        generateRandom() {
-            return Math.floor(Math.random() * 10) + 1;
-        },
-        greetings() {
-            console.log('ho cliccato sul bottone')
-            this.message = 'nuovo messaggio';
-            this.friends.push(this.generateRandom(1, 100))
-            console.log(this.friends)
-        }
+        /*    generateRandom() {
+               return Math.floor(Math.random() * 10) + 1;
+           },
+           greetings() {
+               console.log('ho cliccato sul bottone')
+               this.message = 'nuovo messaggio';
+               this.friends.push(this.generateRandom(1, 100))
+               console.log(this.friends)
+           } */
 
     },
 }).mount('#app')
