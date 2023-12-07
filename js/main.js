e741352a7d6888836bd2219239dd4af1b6be4ddf
uomma <marcelloleone975@gmail.com>
2023-12-07 15:34:26 +0100

    v-if

--- js/main.js
+++ js/main.js
@@ -19,11 +19,30 @@ button.addEventListener('input', function(event){
 
 
 const { createApp } = Vue
-
+//option API
 createApp({
     data() {
         return {
-            message: 'Hello Vue!'
+            message: 'Hello Vue!',
+            subtitle: 'sotto titolo!',
+            friends: ['carlo', 'marco ', 'luca'],
+            awesome: true,
         }
-    }
+    },
+    methods: {
+        /* greetings : function(){
+            return 'ciao'
+        } */
+
+        generateRandom(){
+              return Math.floor(Math.random() * 10) + 1;
+        },
+        greetings() {
+            console.log('ho cliccato sul bottone')
+            this.message = 'nuovo messaggio';
+            this.friends.push(this.generateRandom(1,100))
+            console.log(this. friends)
+        }
+
+    },
 }).mount('#app')
